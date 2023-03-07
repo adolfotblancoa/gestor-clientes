6db274495bfcde43657cd5b8d3e455c348c61d52
adolfotblancoa <adolfotomas.blancoaraujo@usp.ceu.es>
2023-03-07 10:18:02 +0100

    consClien

--- dominio/Agenda.java
+++ dominio/Agenda.java
@@ -1,8 +1,25 @@
 package dominio;
+import java.util.ArrayList;
+import dominio.Cliente;
 public class Agenda {
 
-    public Agenda() {
-        
+    public static Agenda{
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    }
+
+    public void consultarCliente(int dni, ArrayList<Cliente> clientes){
+        clientes.stream()
+        .filter(x->x.getDni()==dni)
+        .forEach(x->System.out.println("El cliente es: " + x.getNombre() + " " + x.getApellido()));
     }
     
 }
+
+
+/*  
+   for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i].getDni() == dni) {
+                System.out.println("El cliente es: " + clientes[i].getNombre() + " " + clientes[i].getApellido());
+            }
+        }
+ */
