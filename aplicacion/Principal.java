c25c59420f86a3622d255eb4406ed1687c1ea167
adolfotblancoa <adolfotomas.blancoaraujo@usp.ceu.es>
2023-03-08 10:04:37 +0100

    clientes

--- aplicacion/Principal.java
+++ aplicacion/Principal.java
@@ -8,9 +8,13 @@ public class Principal {
     public static void main(String args[]){
         ArrayList <Cliente> clientes = new ArrayList<Cliente>();
         Cliente cliente1 = new Cliente(12345678, "Omar", "Aboultaif");
-        Cliente cliente2 = new Cliente(87654321, "Trafalgar", "Law");
+        Cliente cliente2 = new Cliente(87654321, "Trafalgar", "D. Law");
+        Cliente cliente3 = new Cliente(24682468, "Monkey", "D. Luffy");
+        Cliente cliente4 = new Cliente(13579135, "Luka", "Doncic");
         clientes.add(cliente1);
         clientes.add(cliente2);
+        clientes.add(cliente3);
+        clientes.add(cliente4);
         System.out.println("Introduzca el DNI del cliente a buscar: ");
         Scanner sc = new Scanner(System.in);
         int dni = sc.nextInt();
@@ -18,6 +22,7 @@ public class Principal {
         sc.close();
         System.out.println("Listado de clientes:");
         Agenda.listarClientes(clientes);
+
 
     }
     
